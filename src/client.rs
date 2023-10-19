86e5fdd6d607507d7801e3ae3986e8a5d1a019f1
Philpax <me@philpax.me>
2023-10-19 12:07:00 +0200

    Implement basic phase system

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use ambient_api::{
     core::{
         camera::{
@@ -9,13 +11,23 @@ use ambient_api::{
         messages::Frame,
         physics::components::linear_velocity,
         transform::components::{lookat_target, lookat_up, rotation, translation},
+        ui::components::focusable,
     },
+    element::use_entity_component,
+    input::Input,
     prelude::*,
 };
-use packages::{tangent_schema::player::components as pc, this::messages::Input};
+use packages::{
+    tangent_rider_schema::{
+        components::{game_phase, is_ready, start_position},
+        types::GamePhase,
+    },
+    tangent_schema::player::components as pc,
+    this::messages::MarkAsReady,
+};
 
 #[main]
-pub fn main() {
+pub async fn main() {
     let camera_id = PerspectiveInfiniteReverseCamera {
         optional: PerspectiveInfiniteReverseCameraOptional {
             translation: Some(Vec3::ONE * 250.0),
@@ -32,45 +44,210 @@ pub fn main() {
     .with(lookat_up(), Vec3::Z)
     .spawn();
 
+    entity::wait_for_component(entity::synchronized_resources(), start_position())
+        .await
+        .unwrap();
+
+    let mut phase = Phase::Construction(Default::default());
     Frame::subscribe(move |_| {
-        let _ = handle_camera(camera_id);
+        let Some(game_phase) =
+            entity::get_component(entity::synchronized_resources(), game_phase())
+        else {
+            return;
+        };
+
+        phase.tick(game_phase, camera_id);
     });
 
-    handle_input();
+    GameUI.el().spawn_interactive();
 }
 
-fn handle_camera(camera_id: EntityId) -> Option<()> {
-    let player_id = player::get_local();
-    let vehicle_ref = entity::get_component(player_id, pc::vehicle_ref())?;
+#[element_component]
+fn GameUI(hooks: &mut Hooks) -> Element {
+    let Some(phase) = use_entity_component(hooks, entity::synchronized_resources(), game_phase())
+    else {
+        return Element::new();
+    };
 
-    let position = entity::get_component(vehicle_ref, translation())?;
-    let rotation = entity::get_component(vehicle_ref, rotation())?;
-    let speed = entity::get_component(vehicle_ref, linear_velocity())?.length();
-
-    let new_lookat_position = position + rotation * vec3(1.5, 5.4, 1.8);
-    let new_lookat_target = new_lookat_position + rotation * -Vec3::Y;
-
-    entity::set_component(camera_id, translation(), new_lookat_position);
-    entity::set_component(camera_id, lookat_target(), new_lookat_target);
-    entity::set_component(
-        camera_id,
-        fovy(),
-        0.9 + (speed.abs() / 300.0).clamp(0.0, 1.0),
-    );
-
-    Some(())
+    match phase {
+        GamePhase::Construction => ConstructionUI.el(),
+        GamePhase::Play => PlayUI.el(),
+    }
 }
 
-fn handle_input() {
-    let mut last_input = input::get();
+enum Phase {
+    Construction(Construction),
+    Play(Play),
+}
+impl Phase {
+    pub fn tick(&mut self, game_phase: GamePhase, camera_id: EntityId) {
+        let running_phase = match self {
+            Phase::Construction(_) => GamePhase::Construction,
+            Phase::Play(_) => GamePhase::Play,
+        };
 
-    fixed_rate_tick(Duration::from_millis(20), move |_| {
+        if game_phase != running_phase {
+            *self = match game_phase {
+                GamePhase::Construction => Phase::Construction(Default::default()),
+                GamePhase::Play => Phase::Play(Default::default()),
+            }
+        }
+
+        match self {
+            Phase::Construction(p) => p.tick(camera_id),
+            Phase::Play(p) => p.tick(camera_id),
+        }
+    }
+}
+
+pub struct Construction {
+    camera_position: Vec3,
+    camera_yaw: f32,
+    camera_pitch: f32,
+}
+impl Default for Construction {
+    fn default() -> Self {
+        Self {
+            camera_position: entity::get_component(
+                entity::synchronized_resources(),
+                start_position(),
+            )
+            .unwrap_or_default()
+                + vec3(0., 20., 20.),
+            camera_yaw: 0.,
+            camera_pitch: 45f32.to_radians(),
+        }
+    }
+}
+impl Construction {
+    pub fn tick(&mut self, camera_id: EntityId) {
+        let (delta, input) = input::get_delta();
+
+        if input.mouse_buttons.contains(&MouseButton::Right) {
+            self.camera_yaw = (self.camera_yaw + delta.mouse_position.x * 1f32.to_radians()) % PI;
+            self.camera_pitch = (self.camera_pitch + delta.mouse_position.y * 1f32.to_radians())
+                .clamp(-89f32.to_radians(), 89f32.to_radians());
+
+            input::set_cursor_lock(true);
+            input::set_cursor_visible(false);
+        } else {
+            input::set_cursor_lock(false);
+            input::set_cursor_visible(true);
+        }
+
+        let rot = Quat::from_rotation_z(self.camera_yaw) * Quat::from_rotation_x(self.camera_pitch);
+        let movement = [
+            (KeyCode::W, -Vec3::Y),
+            (KeyCode::S, Vec3::Y),
+            (KeyCode::A, -Vec3::X),
+            (KeyCode::D, Vec3::X),
+        ]
+        .iter()
+        .filter(|(key, _)| input.keys.contains(key))
+        .fold(Vec3::ZERO, |acc, (_, dir)| acc + *dir);
+        self.camera_position += rot * movement * 10. * delta_time();
+
+        entity::set_component(camera_id, translation(), self.camera_position);
+        entity::set_component(
+            camera_id,
+            lookat_target(),
+            self.camera_position + rot * -Vec3::Y,
+        );
+    }
+}
+
+#[element_component]
+fn ConstructionUI(hooks: &mut Hooks) -> Element {
+    WindowSized::el([ConstructionSidebar.el()])
+        .init(translation(), vec3(0., 0., 0.5))
+        .with_clickarea()
+        .el()
+        .with(focusable(), hooks.instance_id().to_string())
+        .on_spawned(|_, _id, instance_id| {
+            input::set_focus(instance_id);
+        })
+}
+
+#[element_component]
+fn ConstructionSidebar(hooks: &mut Hooks) -> Element {
+    let is_ready = use_entity_component(hooks, player::get_local(), is_ready()).is_some();
+
+    with_rect(
+        FlowColumn::el([
+            FlowColumn::el([
+                Text::el("Tangent Rider").header_style(),
+                Text::el("Use your money to build a course from the red block to the green block."),
+                Text::el("Everyone else can build, too, so build the best path for *you*!"),
+                Text::el("WASD to move, right-click to look around."),
+                Text::el("Click on an available item to try it out; left-clicking will place it."),
+            ])
+            .with(space_between_items(), 4.0),
+            Button::new("Ready!", move |_| {
+                MarkAsReady.send_server_reliable();
+            })
+            .style(ButtonStyle::Primary)
+            .disabled(is_ready)
+            .el(),
+        ])
+        .with_padding_even(4.0)
+        .with(space_between_items(), 6.0),
+    )
+    .with_margin_even(STREET)
+    .with_background(vec4(0.0, 0.0, 0.0, 0.5))
+}
+
+pub struct Play {
+    last_input: Input,
+    last_input_send_time: Duration,
+}
+impl Default for Play {
+    fn default() -> Self {
+        Self {
+            last_input: input::get(),
+            last_input_send_time: game_time(),
+        }
+    }
+}
+impl Play {
+    pub fn tick(&mut self, camera_id: EntityId) {
+        let _ = self.handle_camera(camera_id);
+        self.handle_input();
+    }
+
+    fn handle_camera(&mut self, camera_id: EntityId) -> Option<()> {
+        let player_id = player::get_local();
+        let vehicle_ref = entity::get_component(player_id, pc::vehicle_ref())?;
+
+        let position = entity::get_component(vehicle_ref, translation())?;
+        let rotation = entity::get_component(vehicle_ref, rotation())?;
+        let speed = entity::get_component(vehicle_ref, linear_velocity())?.length();
+
+        let new_lookat_position = position + rotation * vec3(1.5, 5.4, 1.8);
+        let new_lookat_target = new_lookat_position + rotation * -Vec3::Y;
+
+        entity::set_component(camera_id, translation(), new_lookat_position);
+        entity::set_component(camera_id, lookat_target(), new_lookat_target);
+        entity::set_component(
+            camera_id,
+            fovy(),
+            0.9 + (speed.abs() / 300.0).clamp(0.0, 1.0),
+        );
+
+        Some(())
+    }
+
+    fn handle_input(&mut self) {
+        let now = game_time();
+        if (now - self.last_input_send_time) < Duration::from_millis(20) {
+            return;
+        }
+
         if !input::is_game_focused() {
             return;
         }
 
         let input = input::get();
-        let delta = input.delta(&last_input);
+        let delta = input.delta(&self.last_input);
         let direction = {
             let mut direction = Vec2::ZERO;
             if input.keys.contains(&KeyCode::W) {
@@ -88,13 +265,19 @@ fn handle_input() {
             direction
         };
 
-        Input {
+        packages::this::messages::Input {
             direction,
             jump: input.keys.contains(&KeyCode::Space),
             respawn: delta.keys.contains(&KeyCode::K),
         }
         .send_server_unreliable();
 
-        last_input = input;
-    });
+        self.last_input = input;
+        self.last_input_send_time = now;
+    }
+}
+
+#[element_component]
+fn PlayUI(_hooks: &mut Hooks) -> Element {
+    Element::new()
 }
--- src/server.rs
+++ src/server.rs
@@ -14,14 +14,21 @@ use ambient_api::{
 
 use packages::{
     game_object::components::health,
+    tangent_rider_schema::components::{
+        active_players, alive_player_queue, is_ready, start_position,
+    },
     tangent_schema::{
         player::components as pc,
         vehicle::{components as vc, def::components::is_def},
     },
     tangent_spawner_vehicle::messages::VehicleSpawn,
+    this::messages::MarkAsReady,
 };
 
-use crate::packages::this::messages::Input;
+use crate::packages::{
+    tangent_rider_schema::{components::game_phase, types::GamePhase},
+    this::messages::Input,
+};
 
 #[main]
 pub async fn main() {
@@ -65,12 +72,20 @@ pub async fn main() {
             }
         });
 
-    // When a vehicle despawns, remove the vehicle from the player.
+    // When a vehicle despawns, remove the vehicle from the player, and remove
+    // that player from the alive player queue.
     despawn_query(vc::driver_ref())
         .requires(vc::is_vehicle())
         .bind(|vehicles| {
             for (_vehicle_id, driver_id) in vehicles {
                 entity::remove_component(driver_id, pc::vehicle_ref());
+                entity::mutate_component(
+                    entity::synchronized_resources(),
+                    alive_player_queue(),
+                    |queue| {
+                        queue.retain(|id| *id != driver_id);
+                    },
+                );
             }
         });
 
@@ -127,41 +142,50 @@ pub async fn main() {
         }
     });
 
-    // Wait for vehicle defs to be available and for there to be at least one player, then start the play phase
+    MarkAsReady::subscribe(|ctx, _| {
+        if let Some(player_id) = ctx.client_entity_id() {
+            entity::add_component(player_id, is_ready(), ());
+        }
+    });
+
+    // Wait for vehicle defs to be available and for there to be at least one player, then start the game
     block_until(|| entity::get_all(is_def()).len() > 0 && entity::get_all(is_player()).len() > 0)
         .await;
 
-    start_play_phase();
+    start_game();
 }
 
-fn start_play_phase() {
-    /// The length of a single player slot on the platform in metres.
-    const PLATFORM_WIDTH: f32 = 6.0;
-    const PLAYER_SLOT_LENGTH: f32 = 8.0;
+/// The width of the start platform in metres.
+const PLATFORM_WIDTH: f32 = 6.0;
+/// The length of a single player slot on the platform in metres.
+const PLAYER_SLOT_LENGTH: f32 = 8.0;
 
-    let queue = entity::get_all(is_player());
-    let defs = entity::get_all(is_def());
+fn make_level() {
+    /// The position of the start platform.
+    const START_POSITION: Vec3 = vec3(0., 0., 100.);
 
-    assert!(!defs.is_empty(), "No vehicle defs available");
+    let player_count = entity::get_component(entity::synchronized_resources(), active_players())
+        .unwrap_or_default()
+        .len();
 
-    let start_position = vec3(0., 0., 100.);
     let end_rotation_offset_angle = rand::distributions::Uniform::new_inclusive(-45f32, 45f32)
         .sample(&mut thread_rng())
         .to_radians();
-    let end_position = start_position
+    let end_position = START_POSITION
         + Quat::from_rotation_z(end_rotation_offset_angle) * vec3(0., -100., 0.)
         + vec3(0., 0., (random::<f32>() - 0.5) * 50.);
 
     // Spawn platforms
-    let start_platform_length = PLAYER_SLOT_LENGTH * (queue.len() as f32);
+    let start_platform_length = PLAYER_SLOT_LENGTH * (player_count as f32);
     let _start_platform = Entity::new()
         .with(cube(), ())
         .with(cube_collider(), Vec3::ONE)
         .with(scale(), vec3(PLATFORM_WIDTH, start_platform_length, 0.2))
         .with(
             translation(),
-            start_position + vec3(0., start_platform_length / 2., 0.),
+            START_POSITION + vec3(0., start_platform_length / 2., 0.),
         )
+        .with(color(), vec4(1.0, 0.0, 0.0, 1.0))
         .spawn();
 
     let _end_platform = Entity::new()
@@ -169,16 +193,98 @@ fn start_play_phase() {
         .with(cube_collider(), Vec3::ONE)
         .with(scale(), vec3(PLATFORM_WIDTH, PLATFORM_WIDTH, 0.2))
         .with(translation(), end_position)
+        .with(color(), vec4(0.0, 1.0, 0.0, 1.0))
         .spawn();
 
+    entity::add_component(
+        entity::synchronized_resources(),
+        start_position(),
+        START_POSITION,
+    );
+}
+
+fn start_game() {
+    entity::add_component(
+        entity::synchronized_resources(),
+        active_players(),
+        entity::get_all(is_player()),
+    );
+
+    make_level();
+
+    start_construct_phase();
+}
+
+fn start_construct_phase() {
+    entity::add_component(
+        entity::synchronized_resources(),
+        game_phase(),
+        GamePhase::Construction,
+    );
+
+    // Remove the ready state from all players
+    let players = entity::get_component(entity::synchronized_resources(), active_players())
+        .unwrap_or_default();
+
+    for id in &players {
+        entity::remove_component(*id, is_ready());
+    }
+
+    run_async(async move {
+        block_until(|| {
+            players
+                .iter()
+                .all(|id| entity::has_component(*id, is_ready()))
+        })
+        .await;
+
+        start_play_phase();
+    });
+}
+
+fn start_play_phase() {
+    entity::add_component(
+        entity::synchronized_resources(),
+        game_phase(),
+        GamePhase::Play,
+    );
+
+    let mut active_players =
+        entity::get_component(entity::synchronized_resources(), active_players())
+            .unwrap_or_default();
+    active_players.shuffle(&mut thread_rng());
+
+    let defs = entity::get_all(is_def());
+    let start_position = entity::get_component(entity::synchronized_resources(), start_position())
+        .unwrap_or_default();
+
     // Spawn vehicles on platforms
-    for (i, player_id) in queue.iter().enumerate() {
+    for (i, player_id) in active_players.iter().enumerate() {
         VehicleSpawn {
-            def_id: *defs.choose(&mut thread_rng()).unwrap(),
+            def_id: *defs
+                .choose(&mut thread_rng())
+                .expect("no defs available; this should not be possible"),
             position: start_position + vec3(0., ((i as f32) + 0.5) * PLAYER_SLOT_LENGTH, 0.),
             rotation: Some(Quat::from_rotation_z(0f32.to_radians())),
             driver_id: Some(*player_id),
         }
         .send_local_broadcast(false);
     }
+
+    entity::add_component(
+        entity::synchronized_resources(),
+        alive_player_queue(),
+        active_players,
+    );
+
+    run_async(async move {
+        block_until(|| {
+            entity::get_component(entity::synchronized_resources(), alive_player_queue())
+                .unwrap_or_default()
+                .is_empty()
+        })
+        .await;
+
+        start_construct_phase();
+    });
 }
