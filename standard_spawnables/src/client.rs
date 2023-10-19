532e7e8ffb0c8ed89fa11da2f1a025cca35fef0d
Philpax <me@philpax.me>
2023-10-19 18:14:57 +0200

    Implement basic boost pads

--- standard_spawnables/src/client.rs
+++ standard_spawnables/src/client.rs
@@ -1,4 +1,62 @@
-use ambient_api::prelude::*;
+use ambient_api::{
+    core::{
+        app::components::{game_time, main_scene},
+        rect::components::{line_from, line_to, line_width},
+        rendering::components::{color, double_sided},
+        transform::components::local_to_world,
+    },
+    element::{use_entity_component, use_query},
+    prelude::*,
+};
+use packages::this::components::is_boost_pad;
 
 #[main]
-pub fn main() {}
+pub fn main() {
+    BoostPads.el().spawn_interactive();
+}
+
+#[element_component]
+fn BoostPads(hooks: &mut Hooks) -> Element {
+    let boost_pads = use_query(hooks, is_boost_pad());
+    Group::el(boost_pads.into_iter().map(|(id, _)| BoostPad::el(id)))
+}
+
+#[element_component]
+fn BoostPad(hooks: &mut Hooks, id: EntityId) -> Element {
+    let ltw = use_entity_component(hooks, id, local_to_world()).unwrap_or_default();
+    let time = use_entity_component(hooks, entity::resources(), game_time()).unwrap_or_default();
+    let t = time.as_secs_f32() / 2.0;
+
+    let height = 1.0;
+
+    fn make_chevron(ltw: Mat4, t: f32, height: f32) -> Element {
+        Group::el([
+            make_line(
+                ltw.transform_point3(vec3(-0.5, 0.5 - t, height)),
+                ltw.transform_point3(vec3(0.0, 0.0 - t, height)),
+            ),
+            make_line(
+                ltw.transform_point3(vec3(0.0, 0.0 - t, height)),
+                ltw.transform_point3(vec3(0.5, 0.5 - t, height)),
+            ),
+        ])
+    }
+
+    let time_limit = 0.75;
+
+    Group::el([
+        make_chevron(ltw, t % time_limit, height),
+        make_chevron(ltw, (t + 0.25) % time_limit, height),
+        make_chevron(ltw, (t + 0.5) % time_limit, height),
+    ])
+}
+
+fn make_line(p0: Vec3, p1: Vec3) -> Element {
+    Element::new()
+        .with(main_scene(), ())
+        .with(line_from(), p0)
+        .with(line_to(), p1)
+        .with(line_width(), 0.2)
+        .with(color(), vec4(0.8, 0.3, 0.0, 1.0))
+        .with(double_sided(), true)
+}
--- standard_spawnables/src/server.rs
+++ standard_spawnables/src/server.rs
@@ -2,11 +2,16 @@ use ambient_api::{
     core::{
         physics::components::cube_collider,
         primitives::components::cube,
-        transform::components::{scale, translation},
+        rendering::components::color,
+        transform::components::{rotation, scale, translation},
     },
     prelude::*,
 };
-use packages::tangent_rider_schema::{components::autospinner, concepts::Spawnable};
+use packages::{
+    tangent_rider_schema::{components::autospinner, concepts::Spawnable},
+    tangent_schema::vehicle::components::is_vehicle,
+    this::components::{is_boost_pad, last_boost_time},
+};
 
 #[main]
 pub fn main() {
@@ -84,4 +89,50 @@ pub fn main() {
         }
         .spawn();
     }
+
+    boost_pads();
+}
+
+fn boost_pads() {
+    {
+        let base = Entity::new()
+            .with(cube(), ())
+            .with(translation(), Vec3::Z * -100.)
+            .with(scale(), vec3(5.0, 7.5, 0.2))
+            .with(color(), vec4(0.1, 0.1, 0.1, 1.0))
+            .with(is_boost_pad(), ())
+            .with(last_boost_time(), game_time());
+
+        Spawnable {
+            spawnable_name: "Boost Pad".to_string(),
+            spawnable_cost: 150,
+            spawnable_main_ref: base.clone().with(cube_collider(), Vec3::ONE).spawn(),
+            spawnable_ghost_ref: base.spawn(),
+        }
+        .spawn();
+    }
+
+    // Handle touching boost pads.
+    let boost_pad_query = query((translation(), rotation(), last_boost_time()))
+        .requires(is_boost_pad())
+        .build();
+    query(translation())
+        .requires(is_vehicle())
+        .each_frame(move |vehicles| {
+            let boost_pad = boost_pad_query.evaluate();
+
+            for (vehicle_id, position) in vehicles {
+                if let Some((boost_id, (_, boost_rotation, _))) = boost_pad
+                    .iter()
+                    .find(|(_, (boost_position, _, last_boost_time))| {
+                        boost_position.distance_squared(position) < 6.0f32.powi(2)
+                            && (game_time() - *last_boost_time).as_secs_f32() > 1.0
+                    })
+                    .copied()
+                {
+                    physics::add_force(vehicle_id, boost_rotation * -Vec3::Y * 20000.0);
+                    entity::add_component(boost_id, last_boost_time(), game_time());
+                }
+            }
+        });
 }
